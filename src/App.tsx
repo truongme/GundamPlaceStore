0fa5002b2800e3fb1d591361ef23c73a681ed46b
truongngo <truong.ngo@bmbsoft.com.vn>
2024-08-07 13:21:32 +0700

    Update build base

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./views/Home";
+import Login from "./views/Login";
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route  path="/login" element={<Login/>} />
+      </Routes>
     </div>
   );
 }
